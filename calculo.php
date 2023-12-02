511e4b24c7ca1656fa3ce11967d5e8a9b821fb51
Espadasin <103957016+Espadasin@users.noreply.github.com>
2023-12-02 11:11:08 -0300

    Add files via upload

---
+++ calculo.php
@@ -0,0 +1,19 @@
+<?php
+
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $c = $_POST['c'];
+
+    function bhaskara($a,$b,$c){
+        $delta = ($b*$b) - (4*$a*$c);
+
+        $x1 = (($b*(-1) + sqrt($delta)) / (2*$a));
+        $x2 = (($b*(-1) - sqrt($delta)) / (2*$a));
+
+        return "Delta:" . $delta . "<br>" . "X1: ". $x1 . "<br>". "X2: ". $x2;
+    };
+
+    echo bhaskara($a,$b,$c);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bhaskara</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <form action="calculo.php" method="post">
+        <input type="number" name="a" id="A" placeholder="A">
+        <input type="number" name="b" id="B" placeholder="B">
+        <input type="number" name="c" id="C" placeholder="C">
+
+        <input type="submit" value="Enviar">
+    </form>
+
+</body>
+</html>
